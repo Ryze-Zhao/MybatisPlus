6877fb576cef3760a137fd7ad974f08709b3b019
RyzeZhao <643080112@qq.com>
2019-01-31 17:32:46 +0800

    MybatisPlus模块2：配置UserMapper，明天还需要继续研究XML的配置

--- mybatis-plus-2/src/main/java/com/zhaolearn/mybatisplus2/UserMapper.java
+++ mybatis-plus-2/src/main/java/com/zhaolearn/mybatisplus2/UserMapper.java
@@ -1,6 +1,10 @@
 package com.zhaolearn.mybatisplus2;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends BaseMapper<User> {
+    IPage<User> selectPageVo(Page page, @Param("state") Integer state);
 }
