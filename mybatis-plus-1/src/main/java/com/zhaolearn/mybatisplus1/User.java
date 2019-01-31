d912665a226f3ed3e11ab83709281def10bb448c
RyzeZhao <643080112@qq.com>
2019-01-31 17:11:03 +0800

    MybatisPlus模块1：使用Map和Wrapper查询

--- mybatis-plus-1/src/main/java/com/zhaolearn/mybatisplus1/User.java
+++ mybatis-plus-1/src/main/java/com/zhaolearn/mybatisplus1/User.java
@@ -8,4 +8,5 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+    private String testColumn;
 }
--- mybatis-plus-1/src/test/java/com/zhaolearn/mybatisplus1/MybatisPlus1ApplicationTests.java
+++ mybatis-plus-1/src/test/java/com/zhaolearn/mybatisplus1/MybatisPlus1ApplicationTests.java
@@ -1,10 +1,15 @@
 package com.zhaolearn.mybatisplus1;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,10 +17,26 @@ public class MybatisPlus1ApplicationTests {
     @Autowired
     private UserMapper userMapper;
     @Test
-    public void contextLoads() {
-//        userMapper.selectList(null).stream().forEach(e->System.out.println(e.toString()));
-       System.out.println(userMapper.selectById(1).toString());
+    public void testByNull() {
+
+        userMapper.selectList(null).stream().forEach(e -> System.out.println(e.toString()));
+        //System.out.println(userMapper.selectById(1).toString());
     }
+
+    @Test
+    public void testByMap() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        //精确查询的
+        map.put("test_column", "test1");
+        userMapper.selectByMap(map).stream().forEach(e -> System.out.println(e.toString()));
+    }
+
+    @Test
+    public void testByWrapper(){
+        User user=userMapper.selectById(1);
+        userMapper.selectList(new QueryWrapper<User>().like("test_column","test")).stream().forEach(e -> System.out.println(e.toString()));
+    }
+
 
 }
 
