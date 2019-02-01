849f433fca8c323db348894c46244430be5550b5
RyzeZhao <643080112@qq.com>
2019-02-01 15:06:54 +0800

    MybatisPlus模块3：初始化

---
+++ mybatis-plus-3/src/main/java/com/zhaolearn/mybatisplus3/MybatisPlus3Application.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.mybatisplus3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlus3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlus3Application.class, args);
+    }
+
+}
+
